06c0157fc61c0975cc9ddce0037dd921461c196b
Travis <travis-ci@le-garrec.fr>
2021-01-05 20:39:29 +0000

    Add exemple with restrict for C
    Update from b19913893d158c10e01bf0bed46f5b4b5e284884

---
+++ cpp/attribute/restrict1.c.asm
@@ -0,0 +1,10 @@
+
+Disassembly of section .text:
+
+0000000000000000 <updatePtrs>:
+   0:	endbr64 
+   4:	mov    eax,DWORD PTR [rdx]
+   6:	add    DWORD PTR [rdi],eax
+   8:	mov    eax,DWORD PTR [rdx]
+   a:	add    DWORD PTR [rsi],eax
+   c:	ret    
---
+++ cpp/attribute/restrict2.c.asm
@@ -0,0 +1,9 @@
+
+Disassembly of section .text:
+
+0000000000000000 <updatePtrs>:
+   0:	endbr64 
+   4:	mov    eax,DWORD PTR [rdx]
+   6:	add    DWORD PTR [rdi],eax
+   8:	add    DWORD PTR [rsi],eax
+   a:	ret    
